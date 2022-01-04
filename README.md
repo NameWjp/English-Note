ac5283715a0aee11e5e31205b65a47948cefcd94
wangjp <943375372@qq.com>
2022-01-04 20:12:37 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+## 学习英语笔记 
+在线链接：[学习英语笔记](https://namewjp.github.io/learn-english/)
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    base: '/learn-english/',
+    title: '学习英语笔记',
+    head: [
+        ['meta', { name: 'referrer', content: 'no-referrer' }],
+    ],
+    themeConfig: {
+        sidebar: [
+            ['/', '简介'],
+            ['/句子组成', '句子组成'],
+        ],
+        displayAllHeaders: true,
+        smoothScroll: true,
+    }
+}
