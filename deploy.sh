154fa4c4824f0fea0d76b7456a94487c2dd0be96
王金平 <32758131+NameWjp@users.noreply.github.com>
2022-10-27 22:43:07 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:NameWjp/English-Note.git master:gh-pages
+git push -f git@github.com:NameWjp/English-Note.git main:gh-pages
 
 cd -
